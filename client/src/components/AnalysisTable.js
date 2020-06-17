c9462ecaaac9fde38cf5fde0ab5f7d9c45ea7817
Will <w.doucetk@gmail.com>
2020-06-16 22:46:27 -0400

    no data format fixed

--- client/src/components/AnalysisTable.js
+++ client/src/components/AnalysisTable.js
@@ -4,22 +4,24 @@ const AnalysisTable = ({ nomSecteur, avgPrice, typeMonthVariation }) => {
 
     return (
         <div className="col-12 text-center">
-            <h6>{nomSecteur}</h6>
+            <h4><b>{nomSecteur}</b></h4>
             <h6>Prix moyen</h6>
             {//<h6>Tout type confondu : {avgPrice === 'Aucune donnée' ? avgPrice : (Math.round(avgPrice / 1000) * 1000).toLocaleString() + '$'} </h6>
             }
-            <table className="table table-sm">
-                <tbody>
-                    {typeMonthVariation.map((e) => {
-                        return (
-                            <tr key={e.type}>
-                                <td className="text-left">{e.type}</td>
-                                <td className="text-right">{e.meanPrice.toLocaleString() + '$'}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            {avgPrice === 'Aucune donnée' ? avgPrice : (
+                <table className="table table-sm">
+                    <tbody>
+                        {typeMonthVariation.map((e) => {
+                            return (
+                                <tr key={e.type}>
+                                    <td className="text-left">{e.type}</td>
+                                    <td className="text-right">{e.meanPrice.toLocaleString() + '$'}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }
